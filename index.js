936209a507e6eba688f40aedc3da37987bdbbf0a
Nicholas Powers <njpowers7915@gmail.com>
2018-08-05 21:36:37 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ function getFirstSelector(selector) {
 }
 
 function nestedTarget() {
-  return document.querySelector('target#nested')
+  return document.querySelector('#nested')
 }
 
 function increaseRankBy(n) {
