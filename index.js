208803f35f1d52a3158b7017a6078d3cef7d5b8f
Nicholas Powers <njpowers7915@gmail.com>
2018-08-05 21:47:37 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -15,5 +15,5 @@ function increaseRankBy(n) {
 
 function deepestChild() {
   const doc = document.querySelector('#grand-node')
-  return doc.querySelector('div div div div')
+  return doc.querySelector('div div div div div')
 }
