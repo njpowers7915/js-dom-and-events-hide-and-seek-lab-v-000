c8907f38fa279eab676d7f24d595ed444834d7a3
Nicholas Powers <njpowers7915@gmail.com>
2018-08-05 21:47:45 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,19 @@
+function getFirstSelector(selector) {
+  return document.querySelector(selector)
+}
+
+function nestedTarget() {
+  return document.querySelector('#nested')
+}
+
+function increaseRankBy(n) {
+  const lis = document.querySelectorAll('ul.ranked-list li')
+  for (let i = 0; i < lis.length; i++) {
+    lis[i].textContent = parseInt(lis[i].innerHTML) + n
+  }
+}
+
+function deepestChild() {
+  const doc = document.querySelector('#grand-node')
+  return doc.querySelector('div div div div div')
+}
